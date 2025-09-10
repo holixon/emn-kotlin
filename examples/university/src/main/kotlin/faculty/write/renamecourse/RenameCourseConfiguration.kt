66eecc87dc860dce900609533358341251a5d5b3
Jan Galinski <jan.galinski@holisticon.de>
2025-09-10 11:41:41 +0200

    chore: update M3, replace CommandHandlingModule

--- examples/university/src/main/kotlin/faculty/write/renamecourse/RenameCourseConfiguration.kt
+++ examples/university/src/main/kotlin/faculty/write/renamecourse/RenameCourseConfiguration.kt
@@ -1,11 +1,9 @@
 package io.holixon.emn.example.university.faculty.write.renamecourse
 
 import io.holixon.emn.example.university.faculty.type.course.CourseId
-import org.axonframework.commandhandling.CommandHandlingComponent
-import org.axonframework.configuration.ComponentFactory
+import org.axonframework.commandhandling.configuration.CommandHandlingModule
 import org.axonframework.eventsourcing.configuration.EventSourcedEntityModule
 import org.axonframework.eventsourcing.configuration.EventSourcingConfigurer
-import org.axonframework.modelling.configuration.StatefulCommandHandlingModule
 
 
 fun EventSourcingConfigurer.configureRenameCourse(): EventSourcingConfigurer {
@@ -17,14 +15,13 @@ fun EventSourcingConfigurer.configureRenameCourse(): EventSourcingConfigurer {
       )
 
 
-  val commandHandlingModule = StatefulCommandHandlingModule
+  val commandHandlingModule = CommandHandlingModule
     .named("RenameCourse")
-    .entities()
-    .entity(stateEntity)
     .commandHandlers()
     .annotatedCommandHandlingComponent { RenameCourseCommandHandler() }
   //.commandHandlingComponent(
   //    handlingComponentBuilder
   //  )
-  return this.registerStatefulCommandHandlingModule(commandHandlingModule)
+  return this.registerEntity(stateEntity)
+    .registerCommandHandlingModule(commandHandlingModule)
 }
